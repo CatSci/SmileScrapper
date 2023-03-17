b138c53b32337751bb22185e8bc25395da2606e0
CatSci <atul.yadav@catsci.com>
2023-03-17 12:28:55 +0000

    updated requirements.txt

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import sys
+import streamlit as st
 from smilescraper.exception import CustomException
 from smilescraper.logger import logging
 
+st.header('Smile Scraper Project')
 
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info('Logging started')
-        logging.error('Error divde by 0')
-        raise CustomException(error_msg= e, error_detail= sys)
+cas_no = st.text_input('CAS Number', '57-27-2')
+
+
+
+# if __name__ == "__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info('Logging started')
+#         logging.error('Error divde by 0')
+#         raise CustomException(error_msg= e, error_detail= sys)
