4bd66d901fce847e24fe6a718bd7b8712c951552
CatSci <atul.yadav@catsci.com>
2023-03-22 13:03:27 +0000

    using rest api

--- app.py
+++ app.py
@@ -27,57 +27,83 @@ div.stButton > button:first-child:focus {
 </style>""", unsafe_allow_html=True)
 st.header('Smile Scraper')
 
+uploaded_file = st.file_uploader("Choose a file")
+
+if st.button('Search'):
+    # add try except instead if uploaded_files
+    try:
+        filename = uploaded_file.name
+        if ".xlsx" in filename:
+            df = pd.read_excel(uploaded_file)
+        else:
+            df = pd.read_csv(uploaded_file)
+        
+        df = get_data(df= df) 
+        smile_output = convert_df(df)   
+        # Display the DataFrame in the Streamlit app
+        st.write(df)
+        st.success('Successful')
+        st.download_button(
+            label = 'Download data as CSV',
+            data = smile_output,
+            file_name = 'smiles.csv',
+            mime = 'text/csv',
+        )
+    except:
+        st.error('File not Uplaoded')
+
+
 ########################
 ######## Input #########
 ########################
-cas_no = st.text_input('CAS Number', '57-27-2,102-54-5, 108-88-3')
+# cas_no = st.text_input('CAS Number', '57-27-2,102-54-5, 108-88-3')
 
 
-st.text('')
-colT1,colT2 = st.columns([3,4])
-with colT2:
-    st.write('Or')
+# st.text('')
+# colT1,colT2 = st.columns([3,4])
+# with colT2:
+#     st.write('Or')
 
-uploaded_file = st.file_uploader("Choose a file")
+# uploaded_file = st.file_uploader("Choose a file")
 
 ########################
 #### Search Button #####
 ########################
-if st.button('Search'):
-    if cas_no == '' and uploaded_file is None:
-        st.error('Enter some values or uplaod a file')
-    else:
-        with st.spinner('Processing...'):
-            data = {}
+# if st.button('Search'):
+#     if cas_no == '' and uploaded_file is None:
+#         st.error('Enter some values or uplaod a file')
+#     else:
+#         with st.spinner('Processing...'):
+#             data = {}
 
-            if uploaded_file is None and cas_no != '':
-                st.info('You have entered Cas values')
-                input_cas_list = cas_no.split(',')
-            elif uploaded_file is not None and cas_no == '':
-                st.info('You have Uploaded a file')
-                filename = uploaded_file.name
-                if ".xlsx" in filename:
-                    input_df = pd.read_excel(uploaded_file)
-                else:
-                    input_df = pd.read_csv(uploaded_file)
+#             if uploaded_file is None and cas_no != '':
+#                 st.info('You have entered Cas values')
+#                 input_cas_list = cas_no.split(',')
+#             elif uploaded_file is not None and cas_no == '':
+#                 st.info('You have Uploaded a file')
+#                 filename = uploaded_file.name
+#                 if ".xlsx" in filename:
+#                     input_df = pd.read_excel(uploaded_file)
+#                 else:
+#                     input_df = pd.read_csv(uploaded_file)
                 
-                input_cas_list = input_df['CAS Number'].values
-            else:
-                st.error('Please enter values or upload file not both')
+#                 input_cas_list = input_df['CAS Number'].values
+#             else:
+#                 st.error('Please enter values or upload file not both')
 
-            try:
-                df = get_data(data = data, input_cas_list= input_cas_list)
-                smile_output = convert_df(df)
-                st.success('Successful')
-                st.table(df)
-                st.download_button(
-                    label = 'Download data as CSV',
-                    data = smile_output,
-                    file_name = 'smiles.csv',
-                    mime = 'text/csv',
-                )
-            except Exception as e:
-                raise CustomException(error_msg= e, error_detail= sys)
+#             try:
+#                 df = get_data(data = data, input_cas_list= input_cas_list)
+#                 smile_output = convert_df(df)
+#                 st.success('Successful')
+#                 st.table(df)
+#                 st.download_button(
+#                     label = 'Download data as CSV',
+#                     data = smile_output,
+#                     file_name = 'smiles.csv',
+#                     mime = 'text/csv',
+#                 )
+#             except Exception as e:
+#                 raise CustomException(error_msg= e, error_detail= sys)
 
             
 
--- smilescraper/pubchem.py
+++ smilescraper/pubchem.py
@@ -1,14 +1,15 @@
-from selenium import webdriver
-from selenium.webdriver import ChromeOptions
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium import webdriver
+# from selenium.webdriver import ChromeOptions
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 # import chromedriver_binary
 
 from smilescraper.logger import logging
 from smilescraper.exception import CustomException
+import requests
 import sys
 import pandas as pd
 import streamlit as st
@@ -19,25 +20,6 @@ logging.basicConfig(level=logging.INFO)
 
 WAIT_TIME = 3
 
-def get_driver():
-    """Start Selenium Chrome Driver
-
-    Returns:
-        driver: Chrome Driver
-    """
-    try:
-        # logging.info('Logging Started')
-        # logging.info('Chrome Driver Starting')
-        option = webdriver.ChromeOptions()
-        option.add_argument('--headless')
-        option.add_argument('--no-sandbox')
-        service = Service(ChromeDriverManager(version = "111.0.5563.64").install())
-        driver = webdriver.Chrome(options = option)
-        return driver
-    except Exception as e:
-        # logging.error('Error Occured in loading Driver')
-        raise CustomException(error_msg= e, error_detail= sys)
-
 # def get_driver():
 #     """Start Selenium Chrome Driver
 
@@ -45,35 +27,35 @@ def get_driver():
 #         driver: Chrome Driver
 #     """
 #     try:
-#         logging.info('Chrome Driver Starting')
-#         options = webdriver.ChromeOptions()
-#         options.add_argument('--headless')
-#         options.add_argument('--no-sandbox')
-#         service = Service(ChromeDriverManager(version='111.0.5563.64').install())
-#         driver = webdriver.Chrome(service=service, options=options)
+#         # logging.info('Logging Started')
+#         # logging.info('Chrome Driver Starting')
+#         option = webdriver.ChromeOptions()
+#         option.add_argument('--headless')
+#         option.add_argument('--no-sandbox')
+#         service = Service(ChromeDriverManager(version = "111.0.5563.64").install())
+#         driver = webdriver.Chrome(options = option)
 #         return driver
 #     except Exception as e:
-#         logging.error('Error occurred in loading Driver')
-#         raise e
+#         # logging.error('Error Occured in loading Driver')
+#         raise CustomException(error_msg= e, error_detail= sys)
 
-def find_cas_number_link(start_link, driver):
-    """_summary_
+# # def get_driver():
+# #     """Start Selenium Chrome Driver
 
-    Args:
-        start_link (_type_): _description_
-        driver (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    try:
-        elements = WebDriverWait(driver, WAIT_TIME).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'span.breakword')))
-        cid = elements[1].text
-        n_link = start_link.split('#')[0]
-        link = f"{n_link}compound/{cid}"
-        return link
-    except Exception as e:
-        raise CustomException(error_msg= e, error_detail= sys)
+# #     Returns:
+# #         driver: Chrome Driver
+# #     """
+# #     try:
+# #         logging.info('Chrome Driver Starting')
+# #         options = webdriver.ChromeOptions()
+# #         options.add_argument('--headless')
+# #         options.add_argument('--no-sandbox')
+# #         service = Service(ChromeDriverManager(version='111.0.5563.64').install())
+# #         driver = webdriver.Chrome(service=service, options=options)
+# #         return driver
+# #     except Exception as e:
+# #         logging.error('Error occurred in loading Driver')
+# #         raise e
 
 # def find_cas_number_link(start_link, driver):
 #     """_summary_
@@ -86,90 +68,137 @@ def find_cas_number_link(start_link, driver):
 #         _type_: _description_
 #     """
 #     try:
-#         logging.info(f'Finding CAS number link for {start_link}')
 #         elements = WebDriverWait(driver, WAIT_TIME).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'span.breakword')))
 #         cid = elements[1].text
-#         print("CID {}".format(cid))
 #         n_link = start_link.split('#')[0]
-#         print("n_link {}".format(n_link))
 #         link = f"{n_link}compound/{cid}"
-#         print(link)
 #         return link
 #     except Exception as e:
-#         logging.error(f'Error occurred in finding CAS number link for {start_link}')
-#         raise e
+#         raise CustomException(error_msg= e, error_detail= sys)
 
-def get_smile(data, driver, cas_no):
-    """To get the smile formula.
-    Args:
-        data {dict}: dictionary to store information extracted from website.
-        driver {selenium web driver}: to load the website and extract data.
-    Returns:
-        data {dict}: returns a dictionary of information stored.
-    """
-    try:
-        element = WebDriverWait(driver, WAIT_TIME).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#Canonical-SMILES')))
-        # Get the text of the element
-        text = element.text
-        split_text = text.split()
-        canonical_smiles = split_text[2]
-        data[cas_no] = canonical_smiles
-    except Exception as e:
-        # st.write('[INFO] Smile not found')
-        data[cas_no] = 'None'
-        raise CustomException(error_msg= e, error_detail= sys)
+# # def find_cas_number_link(start_link, driver):
+# #     """_summary_
 
-    return data
+# #     Args:
+# #         start_link (_type_): _description_
+# #         driver (_type_): _description_
+
+# #     Returns:
+# #         _type_: _description_
+# #     """
+# #     try:
+# #         logging.info(f'Finding CAS number link for {start_link}')
+# #         elements = WebDriverWait(driver, WAIT_TIME).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'span.breakword')))
+# #         cid = elements[1].text
+# #         print("CID {}".format(cid))
+# #         n_link = start_link.split('#')[0]
+# #         print("n_link {}".format(n_link))
+# #         link = f"{n_link}compound/{cid}"
+# #         print(link)
+# #         return link
+# #     except Exception as e:
+# #         logging.error(f'Error occurred in finding CAS number link for {start_link}')
+# #         raise e
 
 # def get_smile(data, driver, cas_no):
 #     """To get the smile formula.
-
 #     Args:
 #         data {dict}: dictionary to store information extracted from website.
 #         driver {selenium web driver}: to load the website and extract data.
-
 #     Returns:
 #         data {dict}: returns a dictionary of information stored.
 #     """
 #     try:
-#         logging.info(f'Getting SMILES for {cas_no}')
-#         # element = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#Canonical-SMILES')))
-#         # element = WebDriverWait(driver, WAIT_TIME).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#Canonical-SMILES')))
-        
-#         element = WebDriverWait(driver, WAIT_TIME).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#Canonical-SMILES')))
-#         print(element)
-#         canonical_smiles = element.text.split()[2]
+#         element = WebDriverWait(driver, WAIT_TIME).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#Canonical-SMILES')))
+#         # Get the text of the element
+#         text = element.text
+#         split_text = text.split()
+#         canonical_smiles = split_text[2]
 #         data[cas_no] = canonical_smiles
 #     except Exception as e:
-#         logging.error(f'Error occurred in getting SMILES for {cas_no}')
+#         # st.write('[INFO] Smile not found')
 #         data[cas_no] = 'None'
+#         raise CustomException(error_msg= e, error_detail= sys)
+
 #     return data
 
-def get_data(data, input_cas_list):
-    """_summary_
+# # def get_smile(data, driver, cas_no):
+# #     """To get the smile formula.
 
-    Args:
-        data (_type_): _description_
-        input_cas_list (_type_): _description_
+# #     Args:
+# #         data {dict}: dictionary to store information extracted from website.
+# #         driver {selenium web driver}: to load the website and extract data.
 
-    Returns:
-        _type_: _description_
-    """
-    count = 0
-    for i in input_cas_list:           
-        start_link = "https://pubchem.ncbi.nlm.nih.gov/#query=" + str(i)
+# #     Returns:
+# #         data {dict}: returns a dictionary of information stored.
+# #     """
+# #     try:
+# #         logging.info(f'Getting SMILES for {cas_no}')
+# #         # element = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#Canonical-SMILES')))
+# #         # element = WebDriverWait(driver, WAIT_TIME).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#Canonical-SMILES')))
+        
+# #         element = WebDriverWait(driver, WAIT_TIME).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#Canonical-SMILES')))
+# #         print(element)
+# #         canonical_smiles = element.text.split()[2]
+# #         data[cas_no] = canonical_smiles
+# #     except Exception as e:
+# #         logging.error(f'Error occurred in getting SMILES for {cas_no}')
+# #         data[cas_no] = 'None'
+# #     return data
 
-        driver = get_driver()
-        driver.set_page_load_timeout(60)
-        driver.get(start_link)
+# def get_data(data, input_cas_list):
+#     """_summary_
 
-        link = find_cas_number_link(start_link, driver)
+#     Args:
+#         data (_type_): _description_
+#         input_cas_list (_type_): _description_
 
-        driver.get(link)
-        data = get_smile(data, driver, cas_no= i)
-        st.write(data)
-        count += 1
-        print('{} CAS Number is completed'.format(count))
+#     Returns:
+#         _type_: _description_
+#     """
+#     count = 0
+#     for i in input_cas_list:           
+#         start_link = "https://pubchem.ncbi.nlm.nih.gov/#query=" + str(i)
+
+#         driver = get_driver()
+#         driver.set_page_load_timeout(60)
+#         driver.get(start_link)
+
+#         link = find_cas_number_link(start_link, driver)
+
+#         driver.get(link)
+#         data = get_smile(data, driver, cas_no= i)
+#         st.write(data)
+#         count += 1
+#         print('{} CAS Number is completed'.format(count))
     
-    df = pd.DataFrame(list(data.items()), columns= ['Cas No', 'Smile'])
+#     df = pd.DataFrame(list(data.items()), columns= ['Cas No', 'Smile'])
+#     return df
+
+
+def get_canonical_smiles(cas_number):
+    # Send a GET request to PubChem for the given CAS number
+    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{cas_number}/property/CanonicalSMILES/TXT"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        smiles = response.content.decode().strip()
+        if smiles != "":
+            return smiles
+    return None
+
+def get_data(df):
+    df["SMILES"] = ""
+    progress_bar = st.progress(0)
+    status_text = st.empty()
+    for i, cas_number in df["CAS Number"].items():
+        df.at[i, "SMILES"] = get_canonical_smiles(cas_number)
+        progress = (i + 1) / len(df)
+        progress_bar.progress(progress)
+        status_text.text(f"{int(progress*100)}% Complete")
+
+        if i== 10:
+            break
+
+    
     return df
